35115dc0338a589557cc113449981f39002480cc
Ashley Manson <ashley.manson@hotmail.com>
2015-07-24 14:30:55 +1200

    Tidy up

--- COSC450-Assignment1/Generate-City-Script.py
+++ COSC450-Assignment1/Generate-City-Script.py
@@ -1,3 +1,7 @@
+# File: Generate-City-Script.py
+# Author: Ashley Manson
+# Generates a random city layout in Blender
+
 import bpy
 import random
 import math
@@ -6,13 +10,24 @@ import math
 bpy.ops.object.select_pattern()
 bpy.ops.object.delete()
 
+# Number of tiles for x and y
+TILENUMBER = 10 #Should be an EVEN number
 
-# Field Material
-fieldMaterial = bpy.data.materials.new("fieldMaterial")
-fieldMaterial.diffuse_color = (0, 1 ,0)
-fieldMaterial.diffuse_shader = 'LAMBERT'
-fieldMaterial.diffuse_intensity = 0.1
-fieldMaterial.emit = 0.5
+# Constants for Terrain
+EMPTY = -1
+RIVER = 0
+BUILDING = 1
+PARK = 2
+
+# Stores what is in every tile
+Terrain = [[0 for x in range(TILENUMBER-1)] for x in range(TILENUMBER-1)]
+
+# Park Material
+parkMaterial = bpy.data.materials.new("parkMaterial")
+parkMaterial.diffuse_color = (0, 1 ,0)
+parkMaterial.diffuse_shader = 'LAMBERT'
+parkMaterial.diffuse_intensity = 0.1
+parkMaterial.emit = 0.5
 
 # Road Material
 roadMaterial = bpy.data.materials.new("roadMaterial")
@@ -21,140 +36,133 @@ roadMaterial.diffuse_shader = 'LAMBERT'
 roadMaterial.diffuse_intensity = 0.1
 roadMaterial.emit = 0.5
 
+# Water Material
 waterMaterial = bpy.data.materials.new("waterMaterial")
 waterMaterial.diffuse_color = (0, 0.1, 1)
 waterMaterial.diffuse_shader = 'LAMBERT'
 waterMaterial.diffuse_intensity = 0.1
 waterMaterial.emit = 0.5
 
+# Fills the Terrain array
+def generate_terrain():
+    for row in range (0, TILENUMBER-1):
+        for col in range (0, TILENUMBER-1):
+            chance = random.uniform(0, 1)
+            if chance <= 0.2:
+                Terrain[row][col] = RIVER
+            elif chance <= 0.8:
+                Terrain[row][col] = BUILDING
+            elif chance <= 1.0:
+                Terrain[row][col] = PARK
+            else: #Empty
+                Terrain[row][col] = EMPTY
+    return
+
 # Add a plane as the sea level
-def sea_level():
+def add_water_plane(tileScale):
+    tileRange = tileScale * TILENUMBER
+    
     water = bpy.ops.mesh.primitive_plane_add(location = (0, 0, -0.8))
+    
     bpy.context.selected_objects.clear()
     bpy.context.selected_objects.append(water)
-    bpy.ops.transform.resize(value=(50, 50, 1))
+    
+    bpy.ops.transform.resize(value=(tileRange, tileRange, 1))
+    
     bpy.ops.object.shade_smooth()
     bpy.context.object.data.materials.append(waterMaterial)
+    
     return
 
-# Add some buildings
-def buildings_or_fields():
-    for i in range(-4, 5):
-        for j in range(-4, 5):
-            chance = random.uniform(0, 1)
-            if chance <= 0.9:
-                base_n = random.randint(2, 4)
-                base = bpy.ops.mesh.primitive_cube_add(location = (i*3, j*3, base_n))
-                bpy.context.selected_objects.clear()
-                bpy.context.selected_objects.append(base)
-                bpy.ops.transform.resize(value=(1, 1, base_n))
-                top_n = random.randint(base_n+1, base_n*2)
-                top = bpy.ops.mesh.primitive_cube_add(location = (i*3, j*3, top_n))
-                bpy.context.selected_objects.clear()
-                bpy.context.selected_objects.append(top)
-                bpy.ops.transform.resize(value=(0.8, 0.8, top_n))
+# Renders what is in the Terrain array
+def render_terrain():
+    for row in range (0, TILENUMBER-1):
+        for col in range (0, TILENUMBER-1):
+            
+            tileOffset = (TILENUMBER-2)/2
+            x = (row - tileOffset) * 3
+            y = (col - tileOffset) * 3
+            
+            if Terrain[row][col] == RIVER:
+                #print("R")
+                create_river(x, y)
+            elif Terrain[row][col] == BUILDING:
+                #print("B")
+                create_building(x, y)
+            elif Terrain[row][col] == PARK:
+                #print("P")
+                create_park(x, y)
             else:
-                field = bpy.ops.mesh.primitive_plane_add(location = (i*3, j*3, 0.01))
-                bpy.context.selected_objects.clear()
-                bpy.context.selected_objects.append(field)
-                bpy.ops.transform.resize(value = (1, 1, 1))
-                bpy.ops.object.shade_smooth()
-                bpy.context.object.data.materials.append(fieldMaterial)
+                create_empty()
                 
-            road = bpy.ops.mesh.primitive_plane_add(location = (i*3, j*3, 0))
-            bpy.context.selected_objects.clear()
-            bpy.context.selected_objects.append(road)
-            bpy.ops.transform.resize(value = (1.5, 1.5, 1))
-            bpy.ops.object.shade_smooth()
-            bpy.context.object.data.materials.append(roadMaterial)
     return
 
-# Add some hills
-def hills():
-    for i in range (-4, 5):
-        hill_side = bpy.ops.mesh.primitive_plane_add(location = (i*3, -(5*3)+0.8, -0.7))
-        bpy.context.selected_objects.clear()
-        bpy.context.selected_objects.append(hill_side)
-        bpy.ops.transform.resize(value = (1.5, 1, 1))
-        bpy.ops.transform.rotate(value = (math.radians(45)), axis =(1, 0, 0))
-        
-        hill_side = bpy.ops.mesh.primitive_plane_add(location = (i*3, (5*3)-0.8, -0.7))
-        bpy.context.selected_objects.clear()
-        bpy.context.selected_objects.append(hill_side)
-        bpy.ops.transform.resize(value = (1.5, 1, 1))
-        bpy.ops.transform.rotate(value = (math.radians(-45)), axis =(1, 0, 0))
-        
-        hill_side = bpy.ops.mesh.primitive_plane_add(location = ((5*3)-0.8, i*3, -0.7))
-        bpy.context.selected_objects.clear()
-        bpy.context.selected_objects.append(hill_side)
-        bpy.ops.transform.resize(value = (1, 1.5, 1))
-        bpy.ops.transform.rotate(value = (math.radians(45)), axis =(0, 1, 0))
-        
-        hill_side = bpy.ops.mesh.primitive_plane_add(location = (-(5*3)+0.8, i*3, -0.7))
-        bpy.context.selected_objects.clear()
-        bpy.context.selected_objects.append(hill_side)
-        bpy.ops.transform.resize(value = (1, 1.5, 1))
-        bpy.ops.transform.rotate(value = (math.radians(-45)), axis =(0, 1, 0))
+# Creates a river on an x,y tile
+def create_river(x, y):
+    hill_side = bpy.ops.mesh.primitive_plane_add(location = (x, y+0.8, -0.7))
+    bpy.context.selected_objects.clear()
+    bpy.context.selected_objects.append(hill_side)
+    bpy.ops.transform.resize(value = (1.5, 1, 1))
+    bpy.ops.transform.rotate(value = (math.radians(45)), axis =(1, 0, 0))
+    
+    hill_side = bpy.ops.mesh.primitive_plane_add(location = (x, y-0.8, -0.7))
+    bpy.context.selected_objects.clear()
+    bpy.context.selected_objects.append(hill_side)
+    bpy.ops.transform.resize(value = (1.5, 1, 1))
+    bpy.ops.transform.rotate(value = (math.radians(-45)), axis =(1, 0, 0))
+    
+    hill_side = bpy.ops.mesh.primitive_plane_add(location = (x-0.8, y, -0.7))
+    bpy.context.selected_objects.clear()
+    bpy.context.selected_objects.append(hill_side)
+    bpy.ops.transform.resize(value = (1, 1.5, 1))
+    bpy.ops.transform.rotate(value = (math.radians(45)), axis =(0, 1, 0))
+    
+    hill_side = bpy.ops.mesh.primitive_plane_add(location = (x+0.8, y, -0.7))
+    bpy.context.selected_objects.clear()
+    bpy.context.selected_objects.append(hill_side)
+    bpy.ops.transform.resize(value = (1, 1.5, 1))
+    bpy.ops.transform.rotate(value = (math.radians(-45)), axis =(0, 1, 0))
     return
 
-#cone = bpy.ops.mesh.primitive_cone_add(location = ((5*3)-1.47, (5*3)-1.47, -0.7))
-#bpy.context.selected_objects.clear()
-#bpy.context.selected_objects.append(cone)
-#bpy.ops.transform.resize(value = (1.3, 1.3, 0.7))
-#bpy.ops.transform.rotate(value = (math.radians(-45)), axis =(1, 0, 0))
-
-# http://wiki.blender.org/index.php/Dev:2.5/Py/Scripts/Cookbook/Code_snippets/Meshes
-def createMesh(name, origin, verts, edges, faces):
-    # Create mesh and object
-    me = bpy.data.meshes.new(name+'Mesh')
-    ob = bpy.data.objects.new(name, me)
-    ob.location = origin
-    ob.show_name = True
-    # Link object to scene
-    bpy.context.scene.objects.link(ob)
- 
-    # Create mesh from given verts, edges, faces. Either edges or
-    # faces should be [], or you ask for problems
-    me.from_pydata(verts, edges, faces)
- 
-    # Update mesh with new data
-    me.update(calc_edges=True)
-    return ob
-
-# http://wiki.blender.org/index.php/Dev:2.5/Py/Scripts/Cookbook/Code_snippets/Meshes
-def run(origin):
-    (x,y,z) = (0.707107, 0.258819, 0.965926)
-    x = x + 0.7
-    tz = -0.7
-    verts1 = ((x,x,tz), (x,-x,tz), (-x,-x,tz), (-x,x,tz), (0,0,0.7))
-    faces1 = ((1,0,4), (4,2,1), (4,3,2), (4,0,3), (0,1,2,3))
-    ob1 = createMesh('Solid', origin, verts1, [], faces1)
-    #verts2 = ((x,x,0), (y,-z,0), (-z,y,0))
-    #“edges2 = ((1,0), (1,2), (2,0))
-    #ob2 = createMesh('Edgy', origin, verts2, edges2, [])
- 
-    # Move second object out of the way
-    ob1.select = False
-    #ob2.select = True
-    #bpy.ops.transform.translate(value=(0,2,0))
+# Creates a building on an x,y tile
+def create_building(x, y):
+    base_z = random.randint(2, 4)
+    base = bpy.ops.mesh.primitive_cube_add(location = (x, y, base_z))
+    
+    bpy.context.selected_objects.clear()
+    bpy.context.selected_objects.append(base)
+    
+    bpy.ops.transform.resize(value=(1, 1, base_z))
+    
+    top_z = random.randint(base_z+1, base_z*2)
+    top = bpy.ops.mesh.primitive_cube_add(location = (x, y, top_z))
+    
+    bpy.context.selected_objects.clear()
+    bpy.context.selected_objects.append(top)
+    
+    bpy.ops.transform.resize(value=(0.8, 0.8, top_z))
+    
     return
 
-x = (5 * 3) - 1.5
-y = (5 * 3) - 1.5
-z = -0.7
-run((x, y, z))
-x = -(4 * 3) - 1.5
-y = -(4 * 3) - 1.5
-z = -0.7
-run((x, y, z))
-x = -(4 * 3) - 1.5
-y = (5 * 3) - 1.5
-z = -0.7
-run((x, y, z))
-x = (5 * 3) - 1.5
-y = -(4 * 3) - 1.5
-z = -0.7
-run((x, y, z))
-sea_level()
-buildings_or_fields()
-hills()
+# Creates a park on an x,y tile
+def create_park(x, y):
+    park = bpy.ops.mesh.primitive_plane_add(location = (x, y, 0))
+    
+    bpy.context.selected_objects.clear()
+    bpy.context.selected_objects.append(park)
+    
+    bpy.ops.transform.resize(value = (1, 1, 1))
+    
+    bpy.ops.object.shade_smooth()
+    bpy.context.object.data.materials.append(parkMaterial)
+    
+    return
+
+# Empty Tile
+def create_empty():
+    print("Empty Tile")
+    return
+
+generate_terrain()
+render_terrain()
+add_water_plane(1.5)
