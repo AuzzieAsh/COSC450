14d64857b0304ca22cc583fd04437ab6e08950a3
Ashley Manson <ashley.manson@hotmail.com>
2015-07-28 10:56:48 +1200

    Edit to logging

--- COSC450-Assignment1/GenerateCity.py
+++ COSC450-Assignment1/GenerateCity.py
@@ -8,27 +8,11 @@ import math
 import os
 import time
 
-# Change Based on Where to store GenerateCity.log
-#LOG_FILE_PATH = '/Users/localash/Desktop/COSC450/COSC450-Assignment1'
-LOG_FILE_PATH = 'E:/Users/Ashley/Desktop/COSC450/COSC450-Assignment1'
-
-def log(to_write):
-    log_file.write(to_write)
-    return
-
-# Create/Open file for writing
-log_file = open(LOG_FILE_PATH + '/GenerateCity.log', 'w+')
-log(":: start\n\n")
-
-# Starting time of Script
-SCRIPT_START_TIME = time.time()
-
-# Clear The Screen
-bpy.ops.object.select_pattern()
-bpy.ops.object.delete()
+# Change if to logging should be on or off
+DO_LOGGING = True
 
 # Number of tiles for x and y
-TILENUMBER = 50 #Should be an EVEN number
+TILENUMBER = 10 #Should be an EVEN number
 
 # Constants for Terrain
 EMPTY = -1
@@ -37,7 +21,27 @@ BUILDING = 1
 PARK = 2
 
 # Stores what is in every tile
-Terrain = [[0 for x in range(TILENUMBER-1)] for x in range(TILENUMBER-1)]
+Tiles = [[0 for x in range(TILENUMBER-1)] for y in range(TILENUMBER-1)]
+Terrain = [[0 for x in range(TILENUMBER-1)] for y in range(TILENUMBER-1)]
+
+# Open file for logging
+if DO_LOGGING:
+    LOG_FILE_PATH = bpy.path.abspath("//GenerateCity.log")
+    log_file = open(LOG_FILE_PATH, 'w+')
+
+def log(to_write):
+    if DO_LOGGING:
+        log_file.write(to_write)
+    return
+
+# Clear The Screen
+bpy.ops.object.select_pattern()
+bpy.ops.object.delete()
+
+log(":: start logging\n\n")
+
+# Starting time of Script
+SCRIPT_START_TIME = time.time()
 
 # Park Material
 parkMaterial = bpy.data.materials.new("parkMaterial")
@@ -60,27 +64,66 @@ waterMaterial.diffuse_shader = 'LAMBERT'
 waterMaterial.diffuse_intensity = 0.1
 waterMaterial.emit = 0.5
 
+log("def fill_tile_array()\n")
+
 # Fills the Terrain array
-def generate_terrain():
+def fill_tile_array():
+    log("Filling Tiles array...\n")
+    
     for row in range (0, TILENUMBER-1):
         for col in range (0, TILENUMBER-1):
             chance = random.uniform(0, 1)
             if chance <= 0.2:
-                log(" Terrain[%d][%d] = RIVER\n" % (row, col))
-                Terrain[row][col] = RIVER
+                log(" Tiles[%d][%d] = RIVER\n" % (row, col))
+                Tiles[row][col] = RIVER
             elif chance <= 0.8:
-                log(" Terrain[%d][%d] = BUILDING\n" % (row, col))
-                Terrain[row][col] = BUILDING
+                log(" Tiles[%d][%d] = BUILDING\n" % (row, col))
+                Tiles[row][col] = BUILDING
             elif chance <= 1.0:
-                log(" Terrain[%d][%d] = PARK\n" % (row, col))
-                Terrain[row][col] = PARK
+                log(" Tiles[%d][%d] = PARK\n" % (row, col))
+                Tiles[row][col] = PARK
             else: #Empty
-                log(" Terrain[%d][%d] = EMPTY\n" % (row, col))
-                Terrain[row][col] = EMPTY
+                log(" Tiles[%d][%d] = EMPTY\n" % (row, col))
+                Tiles[row][col] = EMPTY
+    log("Done.\n")
     return
+
+log("def render_tile_array()\n")
+
+# Renders what is in the Terrain array
+def render_tile_array():
+    log("Create from Tiles array...\n")
+    
+    for row in range (0, TILENUMBER-1):
+        for col in range (0, TILENUMBER-1):
+            
+            tileOffset = (TILENUMBER-2)/2
+            x = (row - tileOffset) * 3
+            y = (col - tileOffset) * 3
+            
+            if Tiles[row][col] == RIVER:
+                log(" Tile [%d,%d] RIVER Created\n" % (row, col))
+                create_river(x, y)
+            elif Tiles[row][col] == BUILDING:
+                log(" Tile [%d,%d] BUILDING Created\n" % (row, col))
+                create_building(x, y)
+                create_road(x, y)
+            elif Tiles[row][col] == PARK:
+                log(" Tile [%d,%d] PARK Created\n" % (row, col))
+                create_park(x, y)
+                create_road(x, y)
+            else:
+                create_empty()
+                
+    log("Done.\n")            
+    return
+
+log("def add_water_plane()\n")
 
 # Add a plane as the sea level
 def add_water_plane(tileScale):
+    log("Create Water Plane...\n")
+    
     tileRange = tileScale * TILENUMBER
     
     water = bpy.ops.mesh.primitive_plane_add(location = (0, 0, -0.8))
@@ -93,32 +136,10 @@ def add_water_plane(tileScale):
     bpy.ops.object.shade_smooth()
     bpy.context.object.data.materials.append(waterMaterial)
     
+    log("Done.\n")
     return
 
-# Renders what is in the Terrain array
-def render_terrain():
-    for row in range (0, TILENUMBER-1):
-        for col in range (0, TILENUMBER-1):
-            
-            tileOffset = (TILENUMBER-2)/2
-            x = (row - tileOffset) * 3
-            y = (col - tileOffset) * 3
-            
-            if Terrain[row][col] == RIVER:
-                log(" Tile [%d,%d] = RIVER Created\n" % (row, col))
-                create_river(x, y)
-            elif Terrain[row][col] == BUILDING:
-                log(" Tile [%d,%d] = BUILDING Created\n" % (row, col))
-                create_building(x, y)
-                create_road(x, y)
-            elif Terrain[row][col] == PARK:
-                log(" Tile [%d,%d] = PARK Created\n" % (row, col))
-                create_park(x, y)
-                create_road(x, y)
-            else:
-                create_empty()
-                
-    return
+log("def create_river(x, y)\n")
 
 # Creates a river on an x,y tile
 def create_river(x, y):
@@ -145,7 +166,10 @@ def create_river(x, y):
     bpy.context.selected_objects.append(hill_side)
     bpy.ops.transform.resize(value = (1, 1.5, 1))
     bpy.ops.transform.rotate(value = (math.radians(-45)), axis =(0, 1, 0))
+    
     return
+
+log("def create_building(x, y)\n")
 
 # Creates a building on an x,y tile
 def create_building(x, y):
@@ -167,6 +191,8 @@ def create_building(x, y):
     
     return
 
+log("def create_road(x, y)\n")
+
 # Creates a road at an x,y tile
 def create_road(x, y):
     road = bpy.ops.mesh.primitive_plane_add(location = (x, y, 0))
@@ -180,6 +206,8 @@ def create_road(x, y):
     bpy.context.object.data.materials.append(roadMaterial)
     
     return
+
+log("def create_park(x, y)\n")
     
 # Creates a park on an x,y tile
 def create_park(x, y):
@@ -195,22 +223,16 @@ def create_park(x, y):
     
     return
 
+log("def create_empty(x, y)\n")
+
 # Empty Tile
 def create_empty():
     print("Empty Tile")
     return
 
-log("Filling Terrain array...\n")
-generate_terrain()
-log("Done.\n")
-
-log("Create from Terrain array...\n")
-render_terrain()
-log("Done.\n")
-
-log("Create Water Plane...\n")
+fill_tile_array()
+render_tile_array()
 add_water_plane(1.5)
-log("Done.\n")
 
 # End Script Time
 SCRIPT_END_TIME = time.time()
@@ -223,4 +245,6 @@ seconds = total % 60
 log("Total Time (H:M:S): %.0f:%.0f:%.1f\n" % (hours, minutes, seconds))
 
 log("\n:: end\n")
-log_file.close()
+
+if DO_LOGGING:
+    log_file.close()
