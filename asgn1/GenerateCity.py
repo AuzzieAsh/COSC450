60efce7013afe3ad65bc8f267194439d0b81f717
Ashley Manson <ashley.manson@hotmail.com>
2015-09-04 16:18:07 +1200

    re-added parks to the generation

--- asgn1/GenerateCity.py
+++ asgn1/GenerateCity.py
@@ -10,56 +10,33 @@ import time
 
 DO_LOGGING = True # Change if logging should be on or off
 TILENUMBER = 10 # Number of tiles for x (rows) and y (cols)
-SCRIPT_START_TIME = time.time() # Starting time of Script
 
 # Constants for Terrain
 EMPTY = -1
 BUILDING = 0
 PARK = 1
 RIVER = 2
+BUILDING_WIDE = 3
+BUILDING_LARGE = 4
 
 # Stores what is in every tile
 TILES = [[0 for rows in range(TILENUMBER)] for cols in range(TILENUMBER)]
 #[top, bot, left, right]
 AlleyWay = [[0 for rows in range(TILENUMBER)] for cols in range(TILENUMBER)]
 
-# Open file for logging
-if DO_LOGGING:
-    LOG_FILE_PATH = bpy.path.abspath("//GenerateCity.log")
-    log_file = open(LOG_FILE_PATH, 'w+')
+# Green Material
+greenMaterial = bpy.data.materials.new("greenMaterial")
+greenMaterial.diffuse_color = (0, 1 ,0)
+greenMaterial.diffuse_shader = 'LAMBERT'
+greenMaterial.diffuse_intensity = 0.1
+greenMaterial.emit = 0.5
 
-def log(to_write):
-    if DO_LOGGING:
-        total_time = time.time() - SCRIPT_START_TIME
-        days = total_time // 86400
-        hours = total_time // 3600 % 24
-        minutes = total_time // 60 % 60
-        seconds = total_time % 60
-        log_file.write("[%.0f:%.0f:%.1f] " % (hours, minutes, seconds))
-        log_file.write(to_write)
-        log_file.write("\n")
-        log_file.flush()
-    return
-
-# Clear The Screen
-bpy.ops.object.select_pattern()
-bpy.ops.object.delete()
-
-log("start logging")
-
-# Park Material
-parkMaterial = bpy.data.materials.new("parkMaterial")
-parkMaterial.diffuse_color = (0, 1 ,0)
-parkMaterial.diffuse_shader = 'LAMBERT'
-parkMaterial.diffuse_intensity = 0.1
-parkMaterial.emit = 0.5
-
-# Road Material
-roadMaterial = bpy.data.materials.new("roadMaterial")
-roadMaterial.diffuse_color = (0.1, 0.1, 0.1)
-roadMaterial.diffuse_shader = 'LAMBERT'
-roadMaterial.diffuse_intensity = 0.1
-roadMaterial.emit = 0.5
+# Dark Gray Material
+darkGrayMaterial = bpy.data.materials.new("darkGrayMaterial")
+darkGrayMaterial.diffuse_color = (0.1, 0.1, 0.1)
+darkGrayMaterial.diffuse_shader = 'LAMBERT'
+darkGrayMaterial.diffuse_intensity = 0.1
+darkGrayMaterial.emit = 0.5
 
 # White Material
 whiteMaterial = bpy.data.materials.new("whiteMaterial")
@@ -75,12 +52,32 @@ redMaterial.diffuse_shader = 'LAMBERT'
 redMaterial.diffuse_intensity = 0.1
 redMaterial.emit = 0.9
 
-# Water Material
-waterMaterial = bpy.data.materials.new("waterMaterial")
-waterMaterial.diffuse_color = (0, 0.1, 1)
-waterMaterial.diffuse_shader = 'LAMBERT'
-waterMaterial.diffuse_intensity = 0.1
-waterMaterial.emit = 0.5
+# Blue Material
+blueMaterial = bpy.data.materials.new("blueMaterial")
+blueMaterial.diffuse_color = (0, 0.1, 1)
+blueMaterial.diffuse_shader = 'LAMBERT'
+blueMaterial.diffuse_intensity = 0.1
+blueMaterial.emit = 0.5
+
+# Log to_write to the .log file
+def log(to_write):
+    if DO_LOGGING:
+        total_time = time.time() - SCRIPT_START_TIME
+        days = total_time // 86400
+        hours = total_time // 3600 % 24
+        minutes = total_time // 60 % 60
+        seconds = total_time % 60
+        log_file.write("[%.0f:%.0f:%.1f] " % (hours, minutes, seconds))
+        log_file.write(to_write)
+        log_file.write("\n")
+        log_file.flush()
+    return
+
+# Clear and select an the_object
+def select_object(the_object):
+    bpy.context.selected_objects.clear()
+    bpy.context.selected_objects.append(the_object)
+    return
 
 # Fills the Terrain array
 def fill_tile_array():
@@ -88,22 +85,20 @@ def fill_tile_array():
   
     for row in range(len(TILES)):
         for col in range(len(TILES[0])):
-            
-            chance = random.uniform(0, 1)
-            
-            if chance <= 1.0:
+            alley_chance = []
+            for i in range(4):
+                chance = random.uniform(0, 1)
+                if (chance <= 0.5):
+                    alley_chance.append(1)
+                else:
+                    alley_chance.append(0)
+            AlleyWay[row][col] = alley_chance
+
+            chance = random.uniform(0, 1)            
+            if chance <= 0.9:
                 log("[%d,%d] = BUILDING" % (row, col))
                 TILES[row][col] = BUILDING
-                alley_chance = []
-                for i in range(4):
-                    chance = random.uniform(0, 1)
-                    if (chance <= 0.5):
-                        alley_chance.append(1)
-                    else:
-                        alley_chance.append(0)
-                        
-                AlleyWay[row][col] = alley_chance
-            elif chance <= 0.0:
+            elif chance <= 1.0:
                 log("[%d,%d] = PARK" % (row, col))
                 TILES[row][col] = PARK
             elif chance <= 0.0:
@@ -121,45 +116,46 @@ def fill_tile_array():
 def render_tile_array():
     log("render_tile_array called...")
     
+    tileOffset = (TILENUMBER-1)/2
+    
     for row in range (len(TILES)):
         for col in range (len(TILES[0])):
             
-            tileOffset = (TILENUMBER-1)/2
+            # Calculate x,y offset so tile object appears in correct place        
             x = (row - tileOffset) * 3
             y = (col - tileOffset) * 3
             
             if TILES[row][col] == BUILDING:
-                create_building(x, y, row, col)
+                create_building_park(x, y, row, col, BUILDING)
                 log("[%d,%d] BUILDING Created" % (row, col))
             elif TILES[row][col] == PARK:
-                create_park(x, y)
+                create_building_park(x, y, row, col, PARK)
                 log("[%d,%d] PARK Created" % (row, col))
             elif TILES[row][col] == RIVER:      
                 create_river(x, y, row, col)
                 log("[%d,%d] RIVER Created" % (row, col))
             else: #Empty
-                create_empty()
-                log("[%d,%d] EMPTY Created" % (row, col))
+                log("[%d,%d] EMPTY Not Created" % (row, col))
                 
     log("Done.")
     
     return
 
 # Add a plane as the sea level
-def add_water_plane(tileScale):
+def add_water_plane():
     log("add_water_plane called...")
     
+    tileScale = 2
     tileRange = tileScale * TILENUMBER
     
     water = bpy.ops.mesh.primitive_plane_add(location = (0, 0, -0.8))
     
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(water)
+    select_object(water)
     
     bpy.ops.transform.resize(value=(tileRange, tileRange, 1))
     
     bpy.ops.object.shade_smooth()
-    bpy.context.object.data.materials.append(waterMaterial)
+    bpy.context.object.data.materials.append(blueMaterial)
     
     log("Done.")
     return
@@ -174,40 +170,35 @@ def create_river(x, y, row, col):
     if col + 1 < TILENUMBER-1:
         if TILES[row][col+1] != RIVER:
             hill_side = bpy.ops.mesh.primitive_plane_add(location = (x, y+0.8, -0.7))
-            bpy.context.selected_objects.clear()
-            bpy.context.selected_objects.append(hill_side)
+            select_object(hill_side)
             bpy.ops.transform.resize(value = (1.5, 1, 1))
             bpy.ops.transform.rotate(value = (math.radians(45)), axis =(1, 0, 0))
     else:
         hill_side = bpy.ops.mesh.primitive_plane_add(location = (x, y+0.8, -0.7))
-        bpy.context.selected_objects.clear()
-        bpy.context.selected_objects.append(hill_side)
+        select_object(hill_side)
         bpy.ops.transform.resize(value = (1.5, 1, 1))
         bpy.ops.transform.rotate(value = (math.radians(45)), axis =(1, 0, 0))
     
     if col - 1 >= 0 and Tiles[row][col-1] != RIVER:
         hill_side = bpy.ops.mesh.primitive_plane_add(location = (x, y-0.8, -0.7))
-        bpy.context.selected_objects.clear()
-        bpy.context.selected_objects.append(hill_side)
+        select_object(hill_side)
         bpy.ops.transform.resize(value = (1.5, 1, 1))
         bpy.ops.transform.rotate(value = (math.radians(-45)), axis =(1, 0, 0))
     
     hill_side = bpy.ops.mesh.primitive_plane_add(location = (x-0.8, y, -0.7))
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(hill_side)
+    select_object(hill_side)
     bpy.ops.transform.resize(value = (1, 1.5, 1))
     bpy.ops.transform.rotate(value = (math.radians(45)), axis =(0, 1, 0))
     
     hill_side = bpy.ops.mesh.primitive_plane_add(location = (x+0.8, y, -0.7))
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(hill_side)
+    select_object(hill_side)
     bpy.ops.transform.resize(value = (1, 1.5, 1))
     bpy.ops.transform.rotate(value = (math.radians(-45)), axis =(0, 1, 0))
     
     return
 
 # Creates a building on an x,y tile
-def create_building(x, y, row, col):
+def create_building_park(x, y, row, col, tileType):
     
     scale_b_x = 1
     scale_t_x = 0.8
@@ -218,31 +209,31 @@ def create_building(x, y, row, col):
     move_y = 0
     
     if row == 0 or row == len(TILES)-1:
-        scale_b_x = 0.8
-        scale_t_x = 0.6
+        scale_b_x -= 0.2
+        scale_t_x -= 0.2
         
     if row == 0:
-        move_x = 0.2
+        move_x += 0.2
     elif row == len(TILES)-1:
-        move_x = -0.2
+        move_x -= 0.2
         
-    if col == 0 or col == len(TILES)-1:
-        scale_b_y = 0.8
-        scale_t_y = 0.6
+    if col == 0 or col == len(TILES[0])-1:
+        scale_b_y -= 0.2
+        scale_t_y -= 0.2
         
     if col == 0:
-        move_y = 0.2
-    elif col == len(TILES)-1:
-        move_y = -0.2
+        move_y += 0.2
+    elif col == len(TILES[0])-1:
+        move_y -= 0.2
         
-    if row != 0 and col != 0 and row != len(TILES)-1 and col != len(TILES)-1:
+    if row != 0 and col != 0 and row != len(TILES)-1 and col != len(TILES[0])-1:
         if AlleyWay[row][col][0] != AlleyWay[row][col][1]:
             if AlleyWay[row][col][0] == 1:
-                move_y = 0.1
+                move_y += 0.1
                 scale_b_y += 0.1
                 scale_t_y += 0.1
             elif AlleyWay[row][col][1] == 1:
-                move_y = -0.1
+                move_y -= 0.1
                 scale_b_y += 0.1
                 scale_t_y += 0.1
         elif AlleyWay[row][col][0] == 1:
@@ -251,41 +242,49 @@ def create_building(x, y, row, col):
         
         if AlleyWay[row][col][2] != AlleyWay[row][col][3]:
             if AlleyWay[row][col][2] == 1:
-                move_x = -0.1
+                move_x -= 0.1
                 scale_b_x += 0.1
                 scale_t_x += 0.1
             elif AlleyWay[row][col][3] == 1:
-                move_x = 0.1
+                move_x += 0.1
                 scale_b_x += 0.1
                 scale_t_x += 0.1
         elif AlleyWay[row][col][2] == 1:
             scale_b_x += 0.2
             scale_t_x += 0.2
-            
-    # Create Building
-    base_z = random.randint(2, 4)
-    base = bpy.ops.mesh.primitive_cube_add(location = (x+move_x, y+move_y, base_z))
     
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(base)
+    if tileType == BUILDING:  
+        # Create Building Base
+        base_z = random.randint(2, 4)
+        base = bpy.ops.mesh.primitive_cube_add(location = (x+move_x, y+move_y, base_z))
+        select_object(base)
+        bpy.ops.transform.resize(value=(scale_b_x, scale_b_y, base_z))
+        bpy.ops.object.shade_smooth()
+        bpy.context.object.data.materials.append(redMaterial)
+        
+        # Create Building Top
+        top_z = random.randint(base_z+1, base_z*2)
+        top = bpy.ops.mesh.primitive_cube_add(location = (x+move_x, y+move_y, top_z))
+        select_object(top)
+        bpy.ops.transform.resize(value=(scale_t_x, scale_t_y, top_z))
+        bpy.ops.object.shade_smooth()
+        bpy.context.object.data.materials.append(redMaterial)
     
-    bpy.ops.transform.resize(value=(scale_b_x, scale_b_y, base_z))
-    
-    bpy.ops.object.shade_smooth()
-    bpy.context.object.data.materials.append(redMaterial)
-    
-    top_z = random.randint(base_z+1, base_z*2)
-    top = bpy.ops.mesh.primitive_cube_add(location = (x+move_x, y+move_y, top_z))
-    
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(top)
-    
-    bpy.ops.transform.resize(value=(scale_t_x, scale_t_y, top_z))
-    
-    bpy.ops.object.shade_smooth()
-    bpy.context.object.data.materials.append(redMaterial)
-    
+    elif tileType == PARK:  
+        # Create Park
+        park = bpy.ops.mesh.primitive_plane_add(location = (x+move_x, y+move_y, 0.01))
+        select_object(park)
+        bpy.ops.transform.resize(value = (scale_b_x, scale_b_y, 1))
+        bpy.ops.object.shade_smooth()
+        bpy.context.object.data.materials.append(greenMaterial)
+        
     # Create surrounding path and road
+    create_road_and_path(x, y, row, col)
+    
+    return
+
+def create_road_and_path(x, y, row, col):
+    
     path_length_x = 1.2
     move_x_1 = 0
     move_x_2 = 0
@@ -296,6 +295,7 @@ def create_building(x, y, row, col):
     move_y_2 = 0
     slide_y = 0
     
+    # If one of the edge tile
     if row == 0 or row == len(TILES)-1:
         path_length_x = 1
     if row == 0:
@@ -305,7 +305,7 @@ def create_building(x, y, row, col):
         move_x_2 = -0.4
         slide_x = -0.2
         
-    if col == 0 or col == len(TILES)-1:
+    if col == 0 or col == len(TILES[0])-1:
         path_length_y = 1
     if col == 0:
         move_y_1 = 0.4
@@ -313,18 +313,17 @@ def create_building(x, y, row, col):
     elif col == len(TILES)-1:
         move_y_2 = -0.4
         slide_y = -0.2
-        
-    if row != 0 and col != 0 and row != len(TILES)-1 and col != len(TILES)-1:
+    
+    # If not an edge tile  
+    if row != 0 and col != 0 and row != len(TILES)-1 and col != len(TILES[0])-1:
         if AlleyWay[row][col][0] != AlleyWay[row][col][1]:
             if AlleyWay[row][col][0] == 1:
                 path_length_y += 0.1
-                #move_y_1 += 0.1
                 move_y_2 += 0.2
                 slide_y += 0.1
             elif AlleyWay[row][col][1] == 1:
                 path_length_y += 0.1
                 move_y_1 -= 0.2
-                #move_y_2 -= 0.1
                 slide_y -= 0.1
         elif AlleyWay[row][col][0] == 1:
             path_length_y += 0.2
@@ -335,104 +334,77 @@ def create_building(x, y, row, col):
             if AlleyWay[row][col][2] == 1:
                 path_length_x += 0.1
                 move_x_1 -= 0.2
-                #move_x_2 -= 0.1
                 slide_x -= 0.1
             elif AlleyWay[row][col][3] == 1:
                 path_length_x += 0.1
-                #move_x_1 += 0.1
                 move_x_2 += 0.2
                 slide_x += 0.1
         elif AlleyWay[row][col][2] == 1:
             path_length_x += 0.2
             move_x_1 -= 0.2
             move_x_2 += 0.2
-            
+           
+   # Roads/Ground 
     ground = bpy.ops.mesh.primitive_plane_add(location = (x, y, 0))
-    
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(ground)
-    
+    select_object(ground)
     bpy.ops.transform.resize(value = (1.5, 1.5, 1))
-    
     bpy.ops.object.shade_smooth()
-    bpy.context.object.data.materials.append(roadMaterial)
+    bpy.context.object.data.materials.append(darkGrayMaterial)
     
+    # First Path
     path = bpy.ops.mesh.primitive_plane_add(location = (x+1.1+move_x_2, y+slide_y, 0.01))
-
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(path)
-    
+    select_object(path)
     bpy.ops.transform.resize(value = (0.1, path_length_y, 1))
-    
     bpy.ops.object.shade_smooth()
     bpy.context.object.data.materials.append(whiteMaterial)
     
+    # Second Path
     path = bpy.ops.mesh.primitive_plane_add(location = (x-1.1+move_x_1, y+slide_y, 0.01))
-
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(path)
-    
+    select_object(path)
     bpy.ops.transform.resize(value = (0.1, path_length_y, 1))
-    
     bpy.ops.object.shade_smooth()
     bpy.context.object.data.materials.append(whiteMaterial)
 
+    # Third Path
     path = bpy.ops.mesh.primitive_plane_add(location = (x+slide_x, y+1.1+move_y_2, 0.01))
-
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(path)
-    
+    select_object(path)
     bpy.ops.transform.resize(value = (path_length_x, 0.1, 1))
-    
     bpy.ops.object.shade_smooth()
     bpy.context.object.data.materials.append(whiteMaterial)
     
+    # Forth Path
     path = bpy.ops.mesh.primitive_plane_add(location = (x+slide_x, y-1.1+move_y_1, 0.01))
-
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(path)
-    
+    select_object(path)
     bpy.ops.transform.resize(value = (path_length_x, 0.1, 1))
-    
     bpy.ops.object.shade_smooth()
     bpy.context.object.data.materials.append(whiteMaterial)
     
     return
 
-# Creates a road at an x,y tile
-def create_road(x, y):
+# >>> START EXECUTION <<<
 
-    return
-    
-# Creates a park on an x,y tile
-def create_park(x, y):
-    park = bpy.ops.mesh.primitive_plane_add(location = (x, y, 0.01))
-    
-    bpy.context.selected_objects.clear()
-    bpy.context.selected_objects.append(park)
-    
-    bpy.ops.transform.resize(value = (1, 1, 1))
-    
-    bpy.ops.object.shade_smooth()
-    bpy.context.object.data.materials.append(parkMaterial)
-    
-    return
+# Clear The Screen
+bpy.ops.object.select_pattern()
+bpy.ops.object.delete()
 
-# Empty Tile
-def create_empty():
-    print("Empty Tile")
-    return
+# Open file for logging
+if DO_LOGGING:
+    SCRIPT_START_TIME = time.time() # Starting time of Script
+    LOG_FILE_PATH = bpy.path.abspath("//GenerateCity.log")
+    log_file = open(LOG_FILE_PATH, 'w+')
 
+# Start
+log("start logging")
+
+# Generate the City
 fill_tile_array()
 render_tile_array()
-add_water_plane(2)
-
-bpy.context.selected_objects.clear()
+add_water_plane()
 
 log("Tiles Rows: %d" %len(TILES))
 log("Tiles Cols: %d" %len(TILES[0]))
 
+# End
 log("end logging")
-
 if DO_LOGGING:
     log_file.close()
