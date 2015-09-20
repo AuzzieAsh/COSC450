81a3a7d4d1326149a22a8f4d5bc6822203044759
Ashley Manson <ashley.manson@hotmail.com>
2015-09-20 14:22:08 +1200

    basic kmeans

--- asgn2/asgn2.cpp
+++ asgn2/asgn2.cpp
@@ -2,18 +2,17 @@
 #include <opencv/highgui.h>
 
 #include <vector>
+#include <random>
+#include <climits>
 
 /*
- * Segmentation result storage.
- * 
- * The result is a list of cluster centers (means) and a mapping
- * from pixels to clusters. This mapping is stored as an OpenCV
- * image (segmented). The (x,y) pixel of this image is the index 
- * into means, so the pixel values should be integer values. 
+ Starting point from /home/cshome/s/steven/Public/Segmentation/segmentation.cpp
  */
+
 struct SegmentationResult {
-  std::vector<cv::Vec3b> means;
-  cv::Mat segmented;
+    std::vector<cv::Vec3b> centres; // centre/cluster points
+    cv::Mat segmented; // segmented image result
+    cv::Mat labels; // cluster labels
 };
 
 
@@ -21,9 +20,9 @@ struct SegmentationResult {
  * k-Means algorithm
  *
  * Given an image and a value for k, apply k-means segmentation.
- * 
+ *
  * The result's mean vector should have k values, and each value in the
- * segmented image is an index into this vector (an integer from 0 to k-1). 
+ * segmented image is an index into this vector (an integer from 0 to k-1).
  *
  * The implementation provided is *not* k-means. Rather, it takes the sum
  * of the red, green, and blue values. This gives a value in [0, 3*255],
@@ -31,68 +30,100 @@ struct SegmentationResult {
  */
 SegmentationResult kMeans(const cv::Mat& image, unsigned int k) {
 
-  // Set up the SegmentationResult
-  SegmentationResult result;
-  // Since integers are 4-bytes on most platforms, 32S will store an int
-  result.segmented = cv::Mat(image.size(), CV_32SC1);
-  result.segmented.setTo(0);
-  // This will have k segments
-  result.means.resize(k);
+    // Set up the SegmentationResult
+    SegmentationResult result;
+    // Since integers are 4-bytes on most platforms, 32S will store an int
+    result.segmented = cv::Mat(image.size(), CV_32SC1);
+    result.labels = cv::Mat(image.size(), CV_32SC1);
+    result.segmented.setTo(0);
+    result.labels.setTo(0);
+    result.centres.resize(0); // Ensure it's empty
 
-  // This is not k-means, just splitting on sum of R, G, and B.
-  // So the 'means' are k grey values spread across the range [0,255]
-  for (int s = 0; s < k; ++s) {
-    result.means[s][0] = s*256/k;
-    result.means[s][1] = s*256/k;
-    result.means[s][2] = s*256/k;
-  }
-
-  // The step between segments, which is (3/255) split k ways.
-  int step = (3*255/k);
-
-  // Loop over each pixel in the image
-  for (int x = 0; x < image.size().width; ++x) {
-    for (int y = 0; y < image.size().height; ++y) {
-      // Compute the sum of the colour channels
-      cv::Vec3b colour = image.at<cv::Vec3b>(y,x);
-      int sum = colour[0] + colour[1] + colour[2];
-      // Pixel segment is the number of steps in the sum
-      result.segmented.at<unsigned char>(y,x) = sum/step;
+    // Add k centre/cluster points to result.centres
+    for (int c = 0; c < k; c++) {
+        int rand_x = rand() % image.size().width;
+        int rand_y = rand() % image.size().height;
+        result.centres.push_back(image.at<cv::Vec3b>(rand_y, rand_x));
     }
-  }
 
-  return result;
+    int iterations = 0;
+    bool done = false;
+    while (!done) {
+        for (int x = 0; x < image.size().width; x++) {
+            for (int y = 0; y < image.size().height; y++) {
+                double min_distance = std::numeric_limits<double>::max();
+                for (int c = 0; c < k; c++) {
+
+                    double r = image.at<cv::Vec3b>(y,x)[0] - result.centres[c][0];
+                    double g = image.at<cv::Vec3b>(y,x)[1] - result.centres[c][1];
+                    double b = image.at<cv::Vec3b>(y,x)[2] - result.centres[c][2];
+
+                    double distance = sqrt(pow(r,2) + pow(g,2) + pow(b,2));
+
+                    if (distance < min_distance) {
+                        result.labels.at<unsigned char>(y,x) = c;
+                        min_distance = distance;
+                    }
+                }
+            }
+        }
+        for (int c = 0; c < k; c++) {
+            double r = 0, g = 0, b = 0;
+            int count = 0;
+            for (int x = 0; x < image.size().width; x++) {
+                for (int y = 0; y < image.size().height; y++) {
+                    if (result.labels.at<unsigned char>(y,x) == c) {
+                        r += image.at<cv::Vec3b>(y,x)[0];
+                        g += image.at<cv::Vec3b>(y,x)[1];
+                        b += image.at<cv::Vec3b>(y,x)[2];
+                        count++;
+                    }
+                }
+            }
+            result.centres[c] = cv::Vec3b(r/count, g/count, b/count);
+        }
+        std::cout << "iterations: " << iterations++ << "\n";
+        if (iterations > 10)
+            done = true;
+    }
+
+    return result;
 }
 
-int main (int argc, char *argv[]) {
+int main (int argc, char **argv) {
 
-  // Check that there are enough command line paramters
-  // Arguments are input image, k, then output image.
-  if (argc != 4) {
-    std::cout << "Usage: segmentation <input image> <k> <output image>" << std::endl;
-    exit(1);
-  }
-
-  // Read the image and display it
-  cv::Mat image = cv::imread(argv[1]);
-  cv::namedWindow("Display");
-  cv::imshow("Display", image);
-  cv::waitKey(); // Need to call waitKey to refresh display
-
-  // Segment the image
-  SegmentationResult sr = kMeans(image, atoi(argv[2]));
-
-  // Loop over the image
-  for (int x = 0; x < image.size().width; ++x) {
-    for (int y = 0; y < image.size().height; ++y) {
-      // Set each pixel value to the corresponding mean
-      int index = sr.segmented.at<unsigned char>(y,x);
-      image.at<cv::Vec3b>(y,x) = sr.means[index];
+    // Check that there are enough command line paramters
+    // Arguments are input image, k, then output image.
+    if (argc != 4) {
+        std::cout << "Usage: " << argv[0] << " <input image> <k> <output image>" << std::endl;
+        exit(1);
     }
-  }
-  
-  // Display the result, save it, and then wait for a key press
-  cv::imshow("Display", image);
-  cv::imwrite(argv[3], image);
-  cv::waitKey();
+
+    int k = atoi(argv[2]);
+    // Read the image and display it
+    cv::Mat image = cv::imread(argv[1]);
+    cv::imshow("Original Image", image);
+    cv::waitKey(); // Need to call waitKey to refresh display
+
+    // Segment the image
+    SegmentationResult sr = kMeans(image, k);
+
+    // Loop over the image
+    for (int c = 0; c < k; c++) {
+        for (int x = 0; x < image.size().width; ++x) {
+            for (int y = 0; y < image.size().height; ++y) {
+                if (sr.labels.at<unsigned char>(y,x) == c) {
+                    // Set each pixel value to the corresponding centre/cluster
+                    image.at<cv::Vec3b>(y,x) = sr.centres[c];
+                }
+            }
+        }
+    }
+    
+    // Display the result, save it, and then wait for a key press
+    cv::imwrite(argv[3], image);
+    cv::imshow("Segmented Image", image);
+    cv::waitKey();
+    
+    return 0;
 }
